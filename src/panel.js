f1ec113043adc9a0d237a8a406cb1d66aadabad7
Jaume Sanchez <the.spite@gmail.com>
2016-08-01 18:07:07 +0200

    sync

--- src/panel.js
+++ src/panel.js
@@ -1,364 +1,265 @@
-function f( s ) {
+function f() {
 
 	'use strict';
 
-	( function () {
+	console.log( 'rStats' );
 
-	    // prepare base perf object
-	    if ( typeof window.performance === 'undefined' ) {
-	    	window.performance = {};
-	    }
-
-	    if ( !window.performance.now ) {
-
-	    	var nowOffset = Date.now();
-
-	    	if ( performance.timing && performance.timing.navigationStart ) {
-	    		nowOffset = performance.timing.navigationStart;
-	    	}
-
-	    	window.performance.now = function now () {
-	    		return Date.now() - nowOffset;
-	    	};
-
-	    }
-
-	    if( !window.performance.mark ) {
-	    	window.performance.mark = function(){}
-	    }
-
-	    if( !window.performance.measure ) {
-	    	window.performance.measure = function(){}
-	    }
-
-	    if( typeof window.performance.memory === 'undefined' ) {
-	        performance.memory = {
-	            jsHeapSizeLimit: 0,
-	            usedJSHeapSize: 0,
-	            totalJSHeapSize: 0
-	        }
-	    }
-
-	} )();
-
-	function rGraph() {
-
-	    this.canvas = document.createElement( 'canvas' );
-	    this.ctx = this.canvas.getContext( '2d' );
-	    this.max = 0;
-	    this.current = 0;
-
-	    this.height = 13;
-	    this.width = 200;
-
-	    var c ='#666666';// _def.color ? _def.color : '#666666';
-
-	    this.dotCanvas = document.createElement( 'canvas' );
-	    this.dotCtx = this.dotCanvas.getContext( '2d' );
-	    this.dotCanvas.width = 1;
-	    this.dotCanvas.height = 2 * this.height;
-	    this.dotCtx.fillStyle = '#444444';
-	    this.dotCtx.fillRect( 0, 0, 1, 2 * this.height );
-	    this.dotCtx.fillStyle = c;
-	    this.dotCtx.fillRect( 0, this.height, 1, this.height );
-	    this.dotCtx.fillStyle = '#ffffff';
-	    this.dotCtx.globalAlpha = 0.5;
-	    this.dotCtx.fillRect( 0, this.height, 1, 1 );
-	    this.dotCtx.globalAlpha = 1;
-
-	    this.alarmCanvas = document.createElement( 'canvas' );
-	    this.alarmCtx = this.alarmCanvas.getContext( '2d' );
-	    this.alarmCanvas.width = 1;
-	    this.alarmCanvas.height = 2 * this.height;
-	    this.alarmCtx.fillStyle = '#444444';
-	    this.alarmCtx.fillRect( 0, 0, 1, 2 * this.height );
-	    this.alarmCtx.fillStyle = '#b70000';
-	    this.alarmCtx.fillRect( 0, this.height, 1, this.height );
-	    this.alarmCtx.globalAlpha = 0.5;
-	    this.alarmCtx.fillStyle = '#ffffff';
-	    this.alarmCtx.fillRect( 0, this.height, 1, 1 );
-	    this.alarmCtx.globalAlpha = 1;
-
-	    this.init();
-
+	function _h ( f, pre, post ) {
+		return function () {
+			var args = pre.apply( this, arguments ) || arguments;
+			var res = f.apply( this, args );
+			var r;
+			return post ? ( r = post.apply( this, [ res, args ] ), r ? r : res ) : res;
+		};
 	}
 
-	rGraph.prototype.init = function() {
+	window.contexts = [];
 
-	    this.canvas.width = this.width;
-	    this.canvas.height = this.height;
-	    this.canvas.style.width = this.canvas.width + 'px';
-	    this.canvas.style.height = this.canvas.height + 'px';
-	    this.canvas.className = 'rs-canvas';
-
-	    this.ctx.fillStyle = '#444444';
-	    this.ctx.fillRect( 0, 0, this.canvas.width, this.canvas.height );
-
-	}
-
-	rGraph.prototype.draw = function( v, alarm ) {
-
-	    this.current += ( v - this.current ) * 0.1;
-	    this.max *= 0.99;
-	    if ( this.current > this.max ) this.max = this.current;
-	    this.ctx.drawImage( this.canvas, 1, 0, this.canvas.width - 1, this.canvas.height, 0, 0, this.canvas.width - 1, this.canvas.height );
-	    if ( alarm ) {
-	        this.ctx.drawImage( this.alarmCanvas, this.canvas.width - 1, this.canvas.height - this.current * this.canvas.height / this.max - this.height );
-	    } else {
-	        this.ctx.drawImage( this.dotCanvas, this.canvas.width - 1, this.canvas.height - this.current * this.canvas.height / this.max - this.height );
-	    }
-	}
-
-	function rValue( id, settings ) {
-
-	    this.settings = settings;
-	    this.definition = settings.values[ id ] || {}; 
-		this.id = id;
-		this.started = false;
-		this.time = null;
-		this.value = 0;
-		this.total = 0;
-	    this.averageValue = 0;
-	    this.accumValue = 0;
-	    this.accumStart = performance.now();
-	    this.accumSamples = 0;
-
-	}
-
-	rValue.prototype.start = function() {
-		this.started = true;
-		if( this.settings.userTimingAPI ) {
-	    	performance.mark( this.id + '-start' );
-	    }
-		this.time = performance.now();
-	}
-
-	rValue.prototype.end = function() {
-
-		this.value = performance.now() - this.time;
-		if( this.settings.userTimingAPI ) {
-			performance.mark( this.id + '-end' );
-			if( this.started ) {
-				performance.measure( this.id, this.id + '-start', this.id + '-end' );
+	HTMLCanvasElement.prototype.getContext = _h( HTMLCanvasElement.prototype.getContext,
+		function() {
+			console.log( 'CANVAS')
+		},
+		function( res, args ) {
+			if( args[ 0 ] === 'webgl' || args[ 0 ] === 'experimental-webgl' ) {
+				var queryExt = res.getExtension("EXT_disjoint_timer_query");
+				if( queryExt ) {
+					contexts.push( {
+						gl: res,
+						queryExt: queryExt,
+						queries: [],
+						frames: {}
+					} );
+				}
 			}
 		}
-		this.average( this.value );
+		);
 
-	}
+	var drawCalls = 0;
 
-	rValue.prototype.tick = function() {
-		this.end();
-		this.start();
-	}
+	WebGLRenderingContext.prototype.drawArrays = _h( WebGLRenderingContext.prototype.drawArrays, function () {
+		drawCalls ++;
+		// drawCalls += arguments[ 2 ];
+	} );
+	WebGLRenderingContext.prototype.drawElements = _h( WebGLRenderingContext.prototype.drawElements, function () {
+		drawCalls ++;
+		// drawCalls += arguments[ 1 ];
+	} );
 
-	rValue.prototype.frame = function() {
+	var globalTime = 0;
 
-		var t = performance.now();
-		var e = t - this.time;
-		this.total++;
-		if ( e > 1000 ) {
-			if ( this.definition.interpolate === false ) {
-				this.value = this.total;
-			} else {
-	            this.value = this.total * 1000 / e;
+	for( var j in WebGLRenderingContext.prototype ) {
+		try {
+			if( typeof WebGLRenderingContext.prototype[ j ] === 'function' ){
+				( function( id ) {
+					var time;
+					WebGLRenderingContext.prototype[ j ] = _h(
+						WebGLRenderingContext.prototype[ j ],
+						function() {
+							time = performance.now();
+						},
+						function() {
+							globalTime += performance.now() - time;
+						}
+						);
+				})( j );
+
 			}
-			this.total = 0;
-			this.time = t;
-	        this.average( this.value );
+		} catch( e ) {
+		//console.log( j );
 		}
+	}
+
+	var label = document.createElement( 'div' );
+	label.className = 'label';
+	label.style.position = 'fixed';
+	label.style.left = 0
+	label.style.bottom = 0;
+	label.style.backgroundColor = 'black';
+	label.style.padding = '5px'
+	label.style.color = 'white';
+	label.style.zIndex = 100000000;
+	label.style.pointerEvents = 'none';
+
+	function Acc( length ) {
+
+		this.length = length;
+		this.values = new Array( length )
+		this.ptr = 0;
 
 	}
 
-	rValue.prototype.average = function( v ) {
+	Acc.prototype.push = function( v ) {
 
-	    if ( this.definition.average ) {
-	        this.accumValue += v;
-	        this.accumSamples++;
-	        var t = performance.now();
-	        if ( t - this.accumStart >= ( this.definition.avgMs || 1000 ) ) {
-	            this.averageValue = this.accumValue / this.accumSamples;
-	            this.accumValue = 0;
-	            this.accumStart = t;
-	            this.accumSamples = 0;
-	        }
-	    }
+		this.values[ this.ptr++ % this.length  ] = v;
 
 	}
 
+	Acc.prototype.median = function() {
 
-	rValue.prototype.set = function( value ) {
-		
-		this.value = value;
+		var values = this.values;
+		values.sort( function(a,b) {return a - b;} );
 
-	}
+		var half = Math.floor(values.length/2);
 
-	rValue.prototype.get = function() {
-
-	    return this.definition.average ? this.averageValue : this.value
-	}
-
-	function rView() {
+		if(values.length % 2)
+			return values[half];
+		else
+			return (values[half-1] + values[half]) / 2.0;
 
 	}
 
-	rView.prototype.update = function( keys, values ) {
+	Acc.prototype.mean = function() {
 
-		keys.forEach( function( key ) {
+		var values = this.values;
+		return values.reduce( ( a,b ) => a + b, 0 ) / values.length;
 
-			//console.log( key, values[ key ].value );
+	}
+
+	var query = null;
+	var lastTime = performance.now()
+	var meanLength = 30
+	var extValues = new Acc( meanLength );
+	var etValues = new Acc( meanLength );
+	var rICValues = new Acc( meanLength );
+	var rAFValues = new Acc( meanLength );
+	var WebGLValues = new Acc( meanLength );
+	var frameValues = new Acc( 1 );
+	var res = 1;
+
+	function idle( deadline ) {
+		if( deadline.didTimeout ) debugger;
+		var et = ( 1000 / 60 ) - deadline.timeRemaining();
+		rICValues.push( et );
+	}
+
+	var originalRAF = requestAnimationFrame;
+	var rAFs = [];
+	var oTime = performance.now();
+	var frames = 0;
+	var lastTime = performance.now();
+	var startTime = performance.now();
+
+	var frameId = 0;
+	var disjointFrames = {};
+
+	function process() {
+
+		var time = performance.now();
+
+		originalRAF( process );
+		requestIdleCallback( idle );
+
+		disjointFrames[ frameId ] = { time: 0, queries: 0 };
+
+		contexts.forEach( function( context, id ) {
+
+			var queryExt = context.queryExt,
+			gl = context.gl;
+
+			context.queries.forEach( function( q, i ) {
+				var query = q.query;
+				var available = queryExt.getQueryObjectEXT( query, queryExt.QUERY_RESULT_AVAILABLE_EXT );
+				var disjoint = gl.getParameter( queryExt.GPU_DISJOINT_EXT );
+				if( available && !disjoint ) {
+					var timeElapsed = queryExt.getQueryObjectEXT( query, queryExt.QUERY_RESULT_EXT );
+					context.queries.splice( i, 1 );
+					disjointFrames[ q.frameId ].time += timeElapsed;
+					disjointFrames[ q.frameId ].queries--;
+					if( disjointFrames[ q.frameId ].queries === 0 ) {
+						extValues.push( disjointFrames[ q.frameId ].time );
+						disjointFrames[ q.frameId ] = null;
+					}
+				}
+			} )
+
+			var query = queryExt.createQueryEXT();
+			queryExt.beginQueryEXT( queryExt.TIME_ELAPSED_EXT, query );
+			context.queries.push( { frameId: frameId, query: query } );
+			disjointFrames[ frameId ].queries++;
 
 		} );
 
-	    //console.log( 'frame', values[ 'frame' ].value, 1000 / values[ 'frame' ].value );
+		drawCalls = 0
+		globalTime = 0;
 
-	}
+		rAFs.forEach( function( c, i ) {
+			c();
+			rAFs.splice( i, 1 );
+		} );
 
-	function rCSSView() {
+		WebGLValues.push( globalTime );
 
-	    this.fields = {};
-	    this.groups = {};
-	    this.fieldToGroup = {}
+		contexts.forEach( function( context ) {
+			var queryExt = context.queryExt;
+			queryExt.endQueryEXT( queryExt.TIME_ELAPSED_EXT );
+		} );
 
-	    this.container = document.createElement( 'div' );
-	    this.container.className = 'rstats-container';
-	    this.container.style.position = 'fixed';
-	    this.container.style.backgroundColor = 'black';
-	    this.container.style.left = this.container.style.top = 0;
-	    this.container.style.zIndex = 10000;
-	    this.container.style.color = 'white';
-	    document.body.appendChild( this.container );
+		var et = performance.now() - time;
+		etValues.push( et );
 
-	}
+		rAFValues.push( time - oTime );
+		oTime = time;
 
-	rCSSView.prototype = Object.create( rView.prototype );
-	rCSSView.prototype.constructor = rCSSView;
-
-	rCSSView.prototype.init = function( settings ) {
-
-	    this.settings = settings;
-
-	    for( var j in this.settings.groups ) {
-	        var g = document.createElement( 'div' );
-	        var h = document.createElement( 'h1' );
-	        h.textContent = this.settings.groups[ j ].caption
-	        g.appendChild( h )
-	        this.container.appendChild( g );
-	        this.settings.groups[ j ].values.forEach( function( f ) {
-	            this.fieldToGroup[ f ] = g;
-	        }.bind( this ) )
-	    }
-
-	}
-
-	rCSSView.prototype.addField = function( name ) {
-
-	    var div = document.createElement( 'div' );
-	    var nameSpan = document.createElement( 'span' );
-	    nameSpan.textContent = this.settings.values[ name ] ? ( this.settings.values[ name ].caption ? this.settings.values[ name ].caption : name ) : name;
-	    div.appendChild( nameSpan );
-	    var valueSpan = document.createElement( 'span' );
-	    valueSpan.textContent = '-';
-	    div.appendChild( valueSpan );
-	    var graphSpan = document.createElement( 'span' );
-	    var graphControl = new rGraph();
-	    graphSpan.appendChild( graphControl.canvas )
-	    div.appendChild( graphSpan );
-	    
-	    if( this.fieldToGroup[ name ] ) {
-	        this.fieldToGroup[ name ].appendChild( div );
-	    } else {
-	        this.container.appendChild( div );
-	    }
-
-	    this.fields[ name ] = {
-	        nameSpan: nameSpan,
-	        valueSpan: valueSpan,
-	        graphSpan: graphSpan,
-	        graphControl: graphControl,
-	        units: this.settings.values[ name ] ? ( this.settings.values[ name ].units ? ' ' + this.settings.values[ name ].units : '' ) : ''
-	    }
-
-	}
-
-	rCSSView.prototype.update = function( keys, values ) {
-
-	    keys.forEach( function( key ) {
-
-	        if( !this.fields[ key ] ) {
-	            this.addField( key )
-	        }
-	        var v = values[ key ].get();
-	        this.fields[ key ].valueSpan.textContent = v.toFixed( 2 ) + this.fields[ key ].units;
-	        this.fields[ key ].graphControl.draw( v )
-
-	    }.bind( this ) );
-
-	}
-
-	function rStats( settings ) {
-
-		var id = settings.id || 'rStats';
-
-		var values = {};
-		var keys = [];
-		var views = [];
-
-		function body( id ) {
-
-			id = ( id || 'default' ).toLowerCase();
-			if( values[ id ] ) return values[ id ];
-
-			var v = new rValue( id, settings );
-			values[ id ] = v
-			keys.push( id );
-			return v;
-
+		frames++;
+		if( time > lastTime + 1000 ) {
+			frameValues.push( frames );
+			frames = 0;
+			lastTime = time;
 		}
 
-		body.update = function() {
-
-			views.forEach( function( v ) { v.update( keys, values ) } );
-
-		}
-
-		body.attachView = function( view ) {
-
-	        view.init( settings );
-			views.push( view );
-
-		}
-
-		return body;
+		frameId++;
 
 	}
 
-	window.rStats = rStats
-	window.rView = rView
+	originalRAF( process );
 
-	var rS = rStats( {
-        userTimingAPI: false,
-        values: {
-            frame:   { caption: 'Total frame time', units: 'ms', average: true, avgMs: 100 },
-            fps:     { caption: 'Framerate', units: 'FPS' },
-            raf:     { caption: 'Time since last rAF', units: 'ms', average: true, avgMs: 100 },
-            rstats:  { caption: 'rStats update', units: 'ms', average: true, avgMs: 100 },
-        },
-    } );
+	function updateValues() {
 
-    rS.attachView( new rCSSView() );
+		var raf = rAFValues.median();
+		var timeElapsed = extValues.median() / ( 1000 * 1000 );
+		var et = etValues.median();
+		var rICet = rICValues.median();
+		var comp = timeElapsed + et;
+		var wt = WebGLValues.median();
+		var fps = frameValues.median();
 
-    function frame() {
-	    window.requestAnimationFrame( frame ) ;
-		rS( 'frame' ).start();
-		rS( 'rAF' ).tick();
-    	rS( 'FPS' ).frame();
-    	rS( 'frame' ).end();
-	    rS.update();
+		label.innerHTML = 'FPS: ' + fps.toFixed( 2 ) + '<br/>rAF: ' + raf.toFixed( 2 ) + 'ms ' + ( 1000 / raf ).toFixed( 2 ) + 'FPS<br/>EXT_disjoint_timer_query (GPU): ' + timeElapsed.toFixed( res ) + 'ms ' + ( 1000 / timeElapsed ).toFixed( res ) + 'FPS<br/>frame (CPU): ' + et.toFixed( res ) + 'ms ' + ( 1000 / et ).toFixed( res ) + 'FPS<br/>rIC (CPU): ' + rICet.toFixed( res ) + 'ms ' + ( 1000 / rICet ).toFixed( res ) + 'FPS<br/>Comp (CPU+GPU): ' + comp.toFixed(2 ) + 'ms ' + ( 1000 / comp ).toFixed( 2 ) + 'FPS<br/>WebGL CPU Ex: ' + wt.toFixed( 2 ) + 'ms ' + ( 1000 / wt ).toFixed( 2 ) + 'FPS<br/>drawCalls: ' + drawCalls;
+
 	}
 
-	frame();
+	setInterval( updateValues, 100 );
+
+	requestAnimationFrame = function( c ) {
+
+		rAFs.push( c );
+
+	}
+
+	window.start = function() {
+		document.body.appendChild( label );
+	}
+
+	window.addEventListener( 'load', start );
 
 }
+
+/*
+
+PANEL STARTS HERE
+
+*/
+
+'use strict'
+
+console.log( 'rStats loaded' );
+
+var button = document.getElementById( 'button' );
+button.addEventListener( 'click', function( e ) {
+	chrome.devtools.inspectedWindow.reload( {
+		ignoreCache: true,
+	//injectedScript: '(' + f.toString() + ')()'
+	} );
+} );
 
 var backgroundPageConnection = chrome.runtime.connect({
 	name: 'panel'
@@ -369,14 +270,19 @@ backgroundPageConnection.postMessage({
 	tabId: chrome.devtools.inspectedWindow.tabId
 });
 
-
 backgroundPageConnection.onMessage.addListener( function( msg ) {
 
 	console.log( '>>', msg );
+
 	switch( msg.method ) {
+		case 'inject':
+		console.log( 'inject' )
+		console.log( chrome.devtools.inspectedWindow.eval( '(' + f.toString() + ')({})' ) );
+		chrome.devtools.inspectedWindow.eval( 'start()' );
+		break;
 		case 'loaded':
-			console.log( 'loaded' )
-			console.log( chrome.devtools.inspectedWindow.eval( '(' + f.toString() + ')({})' ) );
-			break;
+		console.log( 'loaded' )
+		chrome.devtools.inspectedWindow.eval( 'start()' );
+		break;
 	}
 } );
